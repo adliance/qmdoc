340dd623915df5459ecb74962d373b1756d47999
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2020-10-05 19:49:33 +0200

    Lets see if this works better for self-updating.

--- src/Adliance.QmDoc/UpdateService.cs
+++ src/Adliance.QmDoc/UpdateService.cs
@@ -11,23 +11,16 @@ namespace Adliance.QmDoc
             var pi = new ProcessStartInfo
             {
                 FileName = "dotnet",
-                Arguments = "tool update -g Adliance.QmDoc --ignore-failed-sources --no-cache"
+                Arguments = "tool update -g Adliance.QmDoc --ignore-failed-sources --no-cache",
+                WindowStyle = ProcessWindowStyle.Normal,
+                UseShellExecute = true,
+                CreateNoWindow = false
             };
             
-            var currentConsoleColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.DarkGray;
             var process = Process.Start(pi);
             if (process == null)
             {
                 throw new Exception("Process is null.");
-            }
-
-            process.WaitForExit();
-            Console.ForegroundColor = currentConsoleColor;
-            
-            if (process.ExitCode != 0)
-            {
-                throw new Exception($"Update failed (exit code {process.ExitCode}.");
             }
         }
     }
