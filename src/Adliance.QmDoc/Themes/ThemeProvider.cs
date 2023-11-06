ac6cbf7466a9779c4eedfda35b0e691658e5b44d
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2023-11-06 15:49:25 +0100

    Add mermaid support, and very basic new theme.

--- src/Adliance.QmDoc/Themes/ThemeProvider.cs
+++ src/Adliance.QmDoc/Themes/ThemeProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Text.Json;
 using Adliance.QmDoc.Options;
+using Microsoft.AspNetCore.Routing;
 
 namespace Adliance.QmDoc.Themes;
 
@@ -8,27 +9,27 @@ public static class ThemeProvider
 {
     public static string GetHeader(string theme)
     {
-        return GetContent(theme, "header.html") ?? GetEmbeddedContent("header.html") ?? "";
+        return GetContent(theme, "header.html") ?? GetEmbeddedContent(theme, "header.html") ?? "";
     }
 
     public static string GetFooter(string theme)
     {
-        return GetContent(theme, "footer.html") ?? GetEmbeddedContent("footer.html") ?? "";
+        return GetContent(theme, "footer.html") ?? GetEmbeddedContent(theme, "footer.html") ?? "";
     }
 
     public static string GetContent(string theme)
     {
-        return GetContent(theme, "index.html") ?? GetEmbeddedContent("index.html") ?? "";
+        return GetContent(theme, "index.html") ?? GetEmbeddedContent(theme, "index.html") ?? "";
     }
-        
+
     public static string GetScss(string theme)
     {
-        return GetContent(theme, "style.scss") ?? GetEmbeddedContent("style.scss") ?? "";
+        return GetContent(theme, "style.scss") ?? GetEmbeddedContent(theme, "style.scss") ?? "";
     }
 
     public static ThemeOptions GetOptions(string theme)
     {
-        var json = GetContent(theme, "options.json") ?? GetEmbeddedContent("options.json");
+        var json = GetContent(theme, "options.json") ?? GetEmbeddedContent(theme, "options.json");
         if (!string.IsNullOrWhiteSpace(json))
         {
             return JsonSerializer.Deserialize<ThemeOptions>(json) ?? new ThemeOptions();
@@ -37,12 +38,18 @@ public static class ThemeProvider
         return new ThemeOptions();
     }
 
-    private static string? GetEmbeddedContent(string fileName)
+    private static string? GetEmbeddedContent(string theme, string fileName)
     {
-        var name = "Adliance.QmDoc.Themes.Default." + fileName;
+        var name = "Adliance.QmDoc.Themes." + theme + "." + fileName;
         var stream = typeof(ThemeProvider).Assembly.GetManifestResourceStream(name);
-        if (stream == null) return null;
 
+        if (stream == null)
+        {
+            name = "Adliance.QmDoc.Themes.Default." + fileName;
+            stream = typeof(ThemeProvider).Assembly.GetManifestResourceStream(name);
+        }
+
+        if (stream == null) return null;
         using var sr = new StreamReader(stream);
         return sr.ReadToEnd();
     }
